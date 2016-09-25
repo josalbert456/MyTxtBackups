0e196d94fef00081929cfd7af512f172622434b6
josalbert456 <josalbert456@gmail.com>
2016-09-26 02:51:11 +0800

    Update CustomTextView.java

--- CustomTextView.java
+++ CustomTextView.java
@@ -1,29 +1,44 @@
-// because of the space stuffs, this method does not apply to some languages
-String result = "";
-int heightFactor = 0;
-int curPos = 0;
-paint.getTextBounds(text, 0, text.length(), bound);
-int viewWidth = this.getWidth();
-while(bound.width()>viewWidth){
-    paint.getTextBounds(text.substring(0, curPos), 0, curPos, bound);
-    curPos++;
-    if(bound.width()>viewWidth){
-        // we have incremented curPos once
-        curPos--;
-        // subtract 2 to deal with the problem when the char at curPos is space
-        result = text.substring(0, curPos-2);
-        result = result.substring(0, result.lastIndexOf(' '));
-        canvas.drawText(result, textPosX, textPosY + fontSize * heightFactor, paint);
-        heightFactor++;
-        text = text.substring(result.length()+1);
-        curPos = 0;
-    }else if(text.substring(0, curPos).indexOf('\n')>=0){
-        canvas.drawText(text.substring(0, curPos), textPosX, textPosY+ fontSize * heightFactor, paint);
-        text = text.substring(curPos);
-
-        curPos = 0;
-        heightFactor++;
+    public static enum LangType{
+        ALPHABETIC, CHARACTER
     }
-    paint.getTextBounds(text, 0, text.length(), bound);
-}
-canvas.drawText(text, textPosX, textPosY + heightFactor * fontSize, paint);
+    LangType langType;
+String result = "";
+        int heightFactor = 0;
+        int curPos = 0;
+        paint.getTextBounds(text, 0, text.length(), bound);
+        while(bound.width()>this.getWidth()){
+            paint.getTextBounds(text.substring(0, curPos), 0, curPos, bound);
+            curPos++;
+            if(bound.width()>this.getWidth()){
+                switch (langType){
+                    case ALPHABETIC:
+                        // we have incremented curPos once
+                        curPos--;
+                        // subtract 2 to deal with the problem when the char at curPos is space
+                        result = text.substring(0, curPos-2);
+                        result = result.substring(0, result.lastIndexOf(' '));
+                        canvas.drawText(result, textPosX, textPosY + fontSize * heightFactor, paint);
+                        heightFactor++;
+                        text = text.substring(result.length()+1);
+                        curPos = 0;
+                        break;
+                    case CHARACTER:
+                        // we have incremented curPos once
+                        curPos--;
+                        result = text.substring(0, curPos-1);
+                        canvas.drawText(result, textPosX, textPosY + fontSize * heightFactor, paint);
+                        heightFactor++;
+                        text = text.substring(result.length());
+                        curPos = 0;
+                        break;
+                }
+            }else if(text.substring(0, curPos).indexOf('\n')>=0){
+                canvas.drawText(text.substring(0, curPos), textPosX, textPosY+ fontSize * heightFactor, paint);
+                text = text.substring(curPos);
+
+                curPos = 0;
+                heightFactor++;
+            }
+            paint.getTextBounds(text, 0, text.length(), bound);
+        }
+        canvas.drawText(text, textPosX, textPosY + heightFactor * fontSize, paint);
